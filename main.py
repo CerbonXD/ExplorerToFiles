5a1d02e49fc3ea6e18624e4ca8634113b8c38891
Felipe Cerboncini Cordeiro <felipecerboncinix@gmail.com>
2025-02-05 15:58:21 -0300

    Make app work when created with pyinstaller

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import threading
 import subprocess
@@ -8,6 +9,7 @@ from win32api import ShellExecute
 from win32gui import PostMessage
 from win32con import WM_CLOSE
 from win32com.client import Dispatch
+import pythoncom
 
 from pystray import MenuItem, Icon
 from PIL import Image
@@ -56,37 +58,53 @@ def launch_files_app(files_aumid, folder_path):
 
 @logger.catch(message="Error processing Explorer window")
 def monitor_explorer_windows():
-    logger.info("Monitoring Explorer windows...")
+    pythoncom.CoInitialize()
+    try:
+        logger.info("Monitoring Explorer windows...")
 
-    files_aumid = get_files_aumid()
-    if files_aumid is None: return
+        files_aumid = get_files_aumid()
+        if files_aumid is None: return
 
-    shell = Dispatch("Shell.Application")
+        shell = Dispatch("Shell.Application")
 
-    while not exit_event.is_set():
-        windows = get_explorer_windows(shell)
+        while not exit_event.is_set():
+            windows = get_explorer_windows(shell)
 
-        for window in windows:
-            hwnd = window.HWND
+            for window in windows:
+                hwnd = window.HWND
 
-            folder_path = get_path_from_window(window)
-            if folder_path is None: continue
-            if folder_path.startswith("::"):
-                folder_path = ""
+                folder_path = get_path_from_window(window)
+                if folder_path is None: continue
+                if folder_path.startswith("::"):
+                    folder_path = ""
 
-            logger.info(f"Detected Explorer window (HWND {hwnd}) with path: {folder_path}")
+                logger.info(f"Detected Explorer window (HWND {hwnd}) with path: {folder_path}")
 
-            launch_files_app(files_aumid, folder_path)
-            logger.info(f"Launched Files app for folder: {folder_path}")
+                launch_files_app(files_aumid, folder_path)
+                logger.info(f"Launched Files app for folder: {folder_path}")
 
-            close_window_by_hwnd(hwnd)
+                close_window_by_hwnd(hwnd)
 
-        time.sleep(0.5)
+            time.sleep(0.5)
+    finally:
+        pythoncom.CoUninitialize()
+
+
+def resource_path(relative_path):
+    """Get absolute path to resource, works for development and for PyInstaller."""
+    try:
+        # PyInstaller creates a temporary folder and stores path in _MEIPASS.
+        base_path = sys._MEIPASS
+
+    except Exception as e:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
 
 
 @logger.catch(message="Error opening custom icon", default=None)
 def create_image():
-    return Image.open("files_redirect_icon.png")
+    return Image.open(resource_path("files_redirect_icon.png"))
 
 
 def on_exit(icon):
